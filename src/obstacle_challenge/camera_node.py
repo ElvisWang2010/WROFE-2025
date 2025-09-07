f169bd0269088234decfa25020dcff445f0c943e
HarryXiao2009 <harrycfxiao@outlook.com>
2025-09-06 22:25:37 -0400

    Update camera_node.py
    changed ros2 stop message system

--- src/obstacle_challenge/camera_node.py
+++ src/obstacle_challenge/camera_node.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from std_msgs.msg import Bool
+from std_msgs.msg import String
 from cv_bridge import CvBridge
 from picamera2 import Picamera2
 import cv2
@@ -9,10 +9,10 @@ import time
 
 
 class CameraNode(Node):
-    def __init__(self):
-        super().__init__('camera_node')
+    def init(self):
+        super().init('camera_node')
         self.publisher = self.create_publisher(Image, '/image_raw', 10)
-        self.subscription = self.create_subscription(Bool, '/stop_signal', self.stop_callback, 10)
+        self.subscription = self.create_subscription(String, '/state', self.state_callback, 10)
         self.bridge = CvBridge()
         self.picam2 = Picamera2()
         self.picam2.start()
@@ -20,8 +20,8 @@ class CameraNode(Node):
 
         self.timer = self.create_timer(0.1, self.publish_frame) 
 
-    def stop_callback(self, msg):
-        if msg.data:
+    def state_callback(self, msg):
+        if msg.data == "stop":
             self.get_logger().info("Lap completed. Stopping camera node.")
             rclpy.shutdown()
 
@@ -45,6 +45,5 @@ def main(args=None):
         node.destroy_node()
         rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__ == 'main':
     main()
-
